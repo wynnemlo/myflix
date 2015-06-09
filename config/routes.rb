607c3c017cb5e19408230b596aaca416d44a07f7
Wynne M Lo <wynnemlo@gmail.com>
2015-06-09 17:18:46 +0800

    Added category model and show category page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
   get '/home', to: 'videos#index'
+
+  resources :videos, only: [:show]
+
+  resources :categories, only: [:show]
 end
