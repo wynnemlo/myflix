c7f9bef51bd1059c25863833c5c033725a88f078
Wynne Lo <wynnemlo@gmail.com>
2015-09-02 11:29:55 +0800

    Fixed ActionMailer bug

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,4 +1,4 @@
-class AppMailer < Action:Mailer::Base
+class AppMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     mail from: 'wynnemlo@gmail.com', to: user.email, subject: "Welcome to MyFlix!"
