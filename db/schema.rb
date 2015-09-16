71cce73eb2cb2dbd6acd37cd3351a646016bf93c
Wynne Lo <wynnemlo@gmail.com>
2015-09-16 21:37:16 +0800

    Added feature test for admin adding videos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150916082447) do
+ActiveRecord::Schema.define(version: 20150916102907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20150916082447) do
     t.integer  "category_id"
     t.string   "large_cover"
     t.string   "small_cover"
+    t.string   "video_url"
   end
 
 end
---
+++ spec/features/admin_adds_new_video.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature 'Admin adds new video' do
+  scenario "admin successfully adds a new video" do
+    comedy = Fabricate(:category, name: "Comedy")
+
+    alice = Fabricate(:admin)
+    sign_in(alice)
+
+    visit new_admin_video_path
+    fill_in "Title", with: "Monk"
+    select "Comedy", from: "Category"
+    fill_in "Description", with: "Awesome show!"
+    attach_file 'Large cover', 'spec/support/uploads/monk_large.jpg'
+    attach_file 'Small cover', 'spec/support/uploads/monk.jpg'
+    fill_in "Video url", with: 'https://s3-ap-southeast-1.amazonaws.com/wynnemlo-myflix/free-loops_Bokeh_Lights_12.mp4'
+    click_button "Add Video"
+
+    sign_out
+    sign_in
+
+    visit video_path(Video.first)
+    expect(page).to have_selector("img[src='/uploads/video/large_cover/1/monk_large.jpg']")
+    expect(page).to have_selector("a[href='https://s3-ap-southeast-1.amazonaws.com/wynnemlo-myflix/free-loops_Bokeh_Lights_12.mp4']")
+  end
+end
