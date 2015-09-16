96dda0fe64720b7ce1322dacba376a0f91a2c745
Wynne Lo <wynnemlo@gmail.com>
2015-09-16 18:16:06 +0800

    Changed seed file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -10,7 +10,7 @@ Myflix::Application.configure do
   config.assets.compress = true
   config.assets.js_compressor = :uglifier
 
-  config.assets.compile = false
+  config.assets.compile = true
 
   config.assets.digest = true
 
